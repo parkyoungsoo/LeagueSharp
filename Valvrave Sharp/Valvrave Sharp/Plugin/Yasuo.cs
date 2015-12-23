050db3be99b7296e05180ddbfe503bd75e79e4e6
parkyoungsoo <asd7247a@naver.com>
2015-12-23 19:16:44 +0900

    Update Yasuo.cs

--- Valvrave Sharp/Valvrave Sharp/Plugin/Yasuo.cs
+++ Valvrave Sharp/Valvrave Sharp/Plugin/Yasuo.cs
@@ -15,6 +15,7 @@
     using LeagueSharp.SDK.Core.Utils;
     using LeagueSharp.SDK.Core.Wrappers;
     using LeagueSharp.SDK.Core.Wrappers.Damages;
+    using LeagueSharp.SDK.Core.Wrappers.TargetSelector;
 
     using SharpDX;
 
